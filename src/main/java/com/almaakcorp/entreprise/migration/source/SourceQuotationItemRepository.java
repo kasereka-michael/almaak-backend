d6a24f972f7cc873f0651315974700743611b8d7
kasereka-michael <kasererekamichael23526@gmail.com>
2025-12-14 14:21:27 +0200

    removed all the migration stuffs

--- src/main/java/com/almaakcorp/entreprise/migration/source/SourceQuotationItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.almaakcorp.entreprise.migration.source;
-
-import com.almaakcorp.entreprise.models.QuotationItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SourceQuotationItemRepository extends JpaRepository<QuotationItem, Long> {}
--- src/main/java/com/almaakcorp/entreprise/migration/source/SourceRepositories.java
+++
@@ -1,4 +0,0 @@
-package com.almaakcorp.entreprise.migration.source;
-
-// Legacy combined file replaced by individual public interfaces.
-// This file can be removed.
